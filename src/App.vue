e21e44d89fcb9054aa5f3a70af15bde7d3f1e94f
Manuela Ienuso <lunaienuso@libero.it>
2022-05-12 15:20:28 +0200

    Create main project and added title

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Emme" src="./assets/Emme pls.gif">
+    <Title msg="La mia prima app con Vue CLI gasp!"></Title>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Title
   }
 }
 </script>
---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{this.msg}}</h1>
+</template>
+
+<script>
+export default {
+  name: `Title`,
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h1 {
+  color: palevioletred;
+  background: rgba(255, 192, 203, 0.418);
+}
+</style>
