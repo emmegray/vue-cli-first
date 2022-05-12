1a427f5d307a8c910c85f2a5a41c217f51a5515e
Manuela Ienuso <lunaienuso@libero.it>
2022-05-12 16:22:18 +0200

    Added Hero components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Emme" src="./assets/Emme pls.gif">
+    <img alt="Emme" src="./assets/Emme pls.gif"/>
     <Title msg="La mia prima app con Vue CLI gasp!"></Title>
+    <Hero msg="Già che ci siamo ecco un jumbotron va'" mgsButton="Beccati anche un buttone a random to'">
+      <img src="./assets/img_jumbotron.png"/>
+    </Hero>
   </div>
 </template>
 
 <script>
 import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components:{
-    Title
+    Title,
+    Hero
   }
 }
 </script>
@@ -24,5 +29,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
+  img {
+    width:auto;
+    max-width: 60%;
+  }
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-jumbotron class="pink d-flex flex-column align-items-center">
+      <p>{{msg}}</p>
+      <slot></slot>
+      <b-button class="pink" href="#">{{mgsButton}}</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: `Hero`,
+  props: {
+    msg: String,
+    mgsButton: String,
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.pink {
+  background: rgba(216, 112, 147, 0.589);
+}
+
+div {
+  p {
+    color: white;
+    font-size: x-large;
+  }
+  .btn {
+    font-size: x-large;
+  }
+}
+</style>
